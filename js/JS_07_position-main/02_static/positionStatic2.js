e0f63c1685a8a579ca276a38a2a20e7c57ebe5d5
komatsubaramai <161417574+komatsubaramai@users.noreply.github.com>
2026-02-21 02:45:02 +0900

    Add files via upload

---
+++ js/JS_07_position-main/02_static/positionStatic2.js
@@ -0,0 +1,8 @@
+console.log(`top:${getComputedStyle(document.getElementById("targetDiv")).top}`);
+console.log(`right:${getComputedStyle(document.getElementById("targetDiv")).right}`);
+console.log(`bottom:${getComputedStyle(document.getElementById("targetDiv")).bottom}`);
+console.log(`left:${getComputedStyle(document.getElementById("targetDiv")).left}`);
+console.log(`height:${getComputedStyle(document.getElementById("targetDiv")).height}`);
+console.log(`zIndex:${getComputedStyle(document.getElementById("targetDiv")).zIndex}`);
+console.log(`width:${getComputedStyle(document.getElementById("targetDiv")).width}`);
+console.log(`fontSize:${getComputedStyle(document.getElementById("targetDiv")).fontSize}`);
---
+++ js/JS_07_position-main/07_absolute/positionAbsolute2.js
@@ -0,0 +1,12 @@
+console.log(`top:${getComputedStyle(document.getElementById("targetDiv")).top}`);
+console.log(`right:${getComputedStyle(document.getElementById("targetDiv")).right}`);
+console.log(`bottom:${getComputedStyle(document.getElementById("targetDiv")).bottom}`);
+console.log(`left:${getComputedStyle(document.getElementById("targetDiv")).left}`);
+console.log(`height:${getComputedStyle(document.getElementById("targetDiv")).height}`);
+console.log(`zIndex:${getComputedStyle(document.getElementById("targetDiv")).zIndex}`);
+console.log(`width:${getComputedStyle(document.getElementById("targetDiv")).width}`);
+console.log(`fontSize:${getComputedStyle(document.getElementById("targetDiv")).fontSize}`);
+console.log(`margin:${getComputedStyle(document.getElementById("parentDiv")).margin}`);
+console.log(`padding:${getComputedStyle(document.getElementById("parentDiv")).padding}`);
+console.log(`borderWidth:${getComputedStyle(document.getElementById("parentDiv")).borderWidth}`);
+
